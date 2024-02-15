d4e5cc36d04893a2fb1fc8b03d90abfda27b6907
id-gxon <wskgw123@gmail.com>
2024-02-15 11:57:10 +0900

    1-8	MemberDAO 생성

---
+++ Community/src/main/java/com/itwillbs/persistence/MemberDAO.java
@@ -0,0 +1,30 @@
+package com.itwillbs.persistence;
+
+import java.util.List;
+
+import com.itwillbs.domain.MemberVO;
+
+/**
+ * DB에서 사용될 동작들을 추상 메서드를 사용해서 정의
+ */
+public interface MemberDAO {
+	// 회원가입 처리 동작
+	public void insertMember(MemberVO vo);
+
+	// 로그인 처리 동작
+	public MemberVO loginMember(MemberVO vo);
+
+	public MemberVO loginMember(String userid, String userpw);
+
+	// 회원정보 조회
+	public MemberVO getMember(String userid);
+
+	// 회원정보 수정
+	public int updateMember(MemberVO uvo);
+
+	// 회원정보 삭제
+	public int deleteMember(MemberVO dvo);
+
+	// 회원목록 조회
+	public List<MemberVO> getMemberList();
+}
---
+++ Community/src/main/java/com/itwillbs/persistence/MemberDAOImpl.java
@@ -0,0 +1,101 @@
+package com.itwillbs.persistence;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import com.itwillbs.domain.MemberVO;
+
+/*
+ * @Repository: 스프링에서 해당 코드를 MemberDAO로 인식하도록 설정
+ * -> root-context.xml에서 해당 객체를 빈(bean)으로 인식 가능
+ */
+@Repository
+public class MemberDAOImpl implements MemberDAO {
+
+	// 1. DB 연결 -> 해당 객체 주입(DI)
+	@Inject
+	private SqlSession sqlSession;
+
+	private static final Logger logger = LoggerFactory.getLogger(MemberDAOImpl.class);
+
+	// mapper의 위치 정보(namespace)
+	private static final String NAMESPACE = "com.itwillbs.mapper.MemberMapper";
+
+	@Override
+	public void insertMember(MemberVO vo) {
+		logger.debug("insertMember(MemberVO vo) 실행");
+		logger.debug("sqlSession 사용 -> mapper 호출");
+
+		sqlSession.insert(NAMESPACE + ".insertMember", vo);
+
+		logger.debug("MySql 실행 완료");
+	}
+
+	@Override
+	public MemberVO loginMember(MemberVO vo) {
+		logger.debug("loginMember(MemberVO vo) 실행");
+
+		MemberVO resultVO = sqlSession.selectOne(NAMESPACE + ".loginMember", vo);
+
+		return resultVO;
+	}
+
+	@Override
+	public MemberVO loginMember(String userid, String userpw) {
+		logger.debug("loginMember(String userid, String userpw) 실행");
+
+		// MemberVO vo = new MemberVO();
+
+		// vo.setUserid(userid);
+		// vo.setUserpw(userpw);
+
+		/*
+		 * 하나의 VO로 전달된 정보가 모두 저장이 불가능한 경우(Join)
+		 * 	=> Map<K, V> 사용
+		 */
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+
+		paramMap.put("userid", userid);
+		paramMap.put("userpw", userpw);
+		// paramMap.put("mapper에서 호출하는 이름", 실제 데이터);
+
+		MemberVO resultVO = sqlSession.selectOne(NAMESPACE + ".loginMember", paramMap);
+
+		return resultVO;
+	}
+
+	@Override
+	public MemberVO getMember(String userid) {
+		logger.debug("getMember(String userid) 실행");
+
+		return sqlSession.selectOne(NAMESPACE + ".getMember", userid);
+	}
+
+	@Override
+	public int updateMember(MemberVO uvo) {
+		logger.debug("updateMember(MemberVO uvo) 실행");
+
+		return sqlSession.update(NAMESPACE + ".updateMember", uvo);
+	}
+
+	@Override
+	public int deleteMember(MemberVO dvo) {
+		logger.debug("deleteMember(MemberVO dvo) 실행");
+
+		return sqlSession.delete(NAMESPACE + ".deleteMember", dvo);
+	}
+
+	@Override
+	public List<MemberVO> getMemberList() {
+
+		return sqlSession.selectList(NAMESPACE + ".getMemberList");
+	}
+}
